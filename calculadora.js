b91452ea75dcb6e901db7eb06c8460e90416024f
luizandrioli <126569800+luizandrioli@users.noreply.github.com>
2024-04-03 09:53:21 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+var n1 = document.querySelector('#numero1')
+var n2 = document.querySelector('#numero2')
+var resultado = document.querySelector('span')
+
+function somar(){
+    resultado.innerHTML = n1.value + n2.value
+}
+
