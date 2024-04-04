adbba4598da6622a6fd1d416bd5a4d06c623f531
luizandrioli <126569800+luizandrioli@users.noreply.github.com>
2024-04-04 09:54:04 -0300

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,35 @@
+//somar//
 var n1 = document.querySelector('#numero1')
 var n2 = document.querySelector('#numero2')
 var resultado = document.querySelector('span')
 
 function somar(){
-    resultado.innerHTML = n1.value + n2.value
+    resultado.innerHTML = parseInt(n1.value) + parseInt(n2.value)
+}
+ 
+//diminuir//
+var n1 = document.querySelector('#numero1')
+var n2 = document.querySelector('#numero2')
+var resultado = document.querySelector('span')
+
+function diminuir(){
+    resultado.innerHTML = parseInt(n1.value) - parseInt(n2.value)
 }
 
+//multiplicar//
+var n1 = document.querySelector('#numero1')
+var n2 = document.querySelector('#numero2')
+var resultado = document.querySelector('span')
+
+function multiplicar(){
+    resultado.innerHTML = parseInt(n1.value) * parseInt(n2.value)
+}
+
+//dividir//
+var n1 = document.querySelector('#numero1')
+var n2 = document.querySelector('#numero2')
+var resultado = document.querySelector('span')
+
+function dividir(){
+    resultado.innerHTML = parseInt(n1.value) / parseInt(n2.value)
+}
